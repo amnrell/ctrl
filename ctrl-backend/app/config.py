5a13865084d8e6ccd85d59a581d80fb807ef755f
amnrell <jjruffin23@gmail.com>
2025-12-09 03:33:36 -0500

    Update config.py

--- ctrl-backend/app/config.py
+++ ctrl-backend/app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Settings:
     PROJECT_NAME = "CTRL Backend"
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL")          # Loaded from .env, NOT hardcoded
     FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
 
 settings = Settings()
