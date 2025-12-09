60ff770c3c4763514122a727e68702d0ad820909
amnrell <jjruffin23@gmail.com>
2025-12-09 03:24:26 -0500

    Update config.py

--- ctrl-backend/app/config.py
+++ ctrl-backend/app/config.py
@@ -8,3 +8,9 @@ class Settings:
     FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
 
 settings = Settings()
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg://ctrl_user:YOUR_PASSWORD@136.111.82.119:5432/ctrl_db"
+)
